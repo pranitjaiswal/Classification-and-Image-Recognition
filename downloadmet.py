5b9c879f5368a426cb93fc245b96a25a4d28ffb4
m0usedrag0n <m0usedrag0n@users.noreply.github.com>
2018-02-11 21:31:15 +0530

    Comments added

--- downloadmet.py
+++ downloadmet.py
@@ -1,8 +1,8 @@
 import urllib.request
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup #library to scrape webpages
 import sys
 
-
+#function to find substring in a string between two given substrings
 def find_between( s, first, last ):
     try:
         start = s.index( first ) + len( first )
@@ -13,12 +13,12 @@ def find_between( s, first, last ):
 
 
 for line in sys.stdin.readlines():
-	idno,pagelink=line.split(';')
-	r = urllib.request.urlopen('http'+pagelink).read()
-	soup = BeautifulSoup(r,"html.parser")
+	idno,pagelink=line.split(';') #split CSV link data into id and link
+	r = urllib.request.urlopen('http'+pagelink).read() #HTTP request to get link code
+	soup = BeautifulSoup(r,"html.parser") #variable storing link code
 	#print(soup.prettify())
-	url = soup.find_all("li", class_="utility-menu__item utility-menu__item--download")
+	url = soup.find_all("li", class_="utility-menu__item utility-menu__item--download") #to find particular li item of the image link
 	#print(url)
-	hreftext=url[0].a['href']
-	imglink=find_between(hreftext,"'","'")
+	hreftext=url[0].a['href'] #to find href content of 'Download' link for image
+	imglink=find_between(hreftext,"'","'") #cleans image URL
 	print(imglink)
