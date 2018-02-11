a6957df1dc181785a770725f0389c19ed0ba4f71
m0usedrag0n <m0usedrag0n@users.noreply.github.com>
2018-02-11 21:28:48 +0530

    Comments added

--- filterdb.py
+++ filterdb.py
@@ -1,7 +1,7 @@
 import sys
 import csv
 
-
+#function to find substring from a string between two given substrings
 def find_between( s, first, last ):
     try:
         start = s.index( first ) + len( first )
@@ -10,13 +10,14 @@ def find_between( s, first, last ):
     except ValueError:
         return ""
 
+#open file given at command line and remove only relevant links
 with open(sys.argv[1],'rb') as f:
 	reader=csv.reader(f)
 	i=0
 	for row in reader:
-		if row[2] !='False':
-			if 'India' in '+'.join(row):
-				if not('United' in row[31]):
+		if row[2] !='False':  #only public domain images
+			if 'India' in '+'.join(row):  #containing word 'India'
+				if not('United' in row[31]):  #excluding word 'United' to remove references to Native Indians in United States
 					url=find_between('+'.join(row), 'http','+')
 					print(row[0]+';'+url)
 					#i=i+1
