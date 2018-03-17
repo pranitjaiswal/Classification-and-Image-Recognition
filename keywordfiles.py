de2598f00d69781bb9e15d732741688eb6326a00
m0usedrag0n <m0usedrag0n@users.noreply.github.com>
2018-03-17 13:15:54 +0530

    Add files via upload

---
+++ keywordfiles.py
@@ -0,0 +1,11 @@
+import sys
+
+for line in sys.stdin:
+	link,keywords = line.split('@')
+	words = keywords.split(';')
+	name=link.split('/')[-1]
+	print('Creating '+name.strip()+'.txt')
+	file = open('txtfiles/'+name+'.txt','a')
+	for word in words:
+		word = word.strip()
+		file.write(word+'\n')
